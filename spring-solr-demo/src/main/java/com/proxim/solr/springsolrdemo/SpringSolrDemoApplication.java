413c5708d5aba7112944d6b333199b4ee2ffd4fa
AlokPal88 <alok.zenith88@gmail.com>
2018-02-05 22:54:25 -0600

    Add files via upload

---
+++ spring-solr-demo/src/main/java/com/proxim/solr/springsolrdemo/SpringSolrDemoApplication.java
@@ -0,0 +1,12 @@
+package com.proxim.solr.springsolrdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSolrDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSolrDemoApplication.class, args);
+	}
+}
